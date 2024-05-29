363e66cf6489a6246deac78893f4aaedc7e3d700
vamsivarra <121616113+vamsivarra@users.noreply.github.com>
2024-05-29 15:31:03 +0100

    first commit

---
+++ ExtentionMethod/Em.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace ExtentionMethod
+{
+    public static class IntExtensions
+    {
+        public static bool IsGreaterThan(this int i, int value)
+        {
+            return i > value;
+        }
+    }
+}
---
+++ ExtentionMethod/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+namespace ExtentionMethod
+{
+	public class Program
+	{
+        public Program()
+        {
+        }
+            static void Main(string[] args)
+            {
+                int i = 10;
+
+                bool result = i.IsGreaterThan(100);
+
+                Console.WriteLine(result);
+            }
+    }
+}
+
+
